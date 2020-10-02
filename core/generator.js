12a8fa3c35ba63cf71c66a6c2325d96bb608a161
Sandeep S <dxsandeeps@gmail.com>
2020-10-02 17:06:05 +0530

    create .env

--- core/generator.js
+++ core/generator.js
@@ -5,7 +5,9 @@ const srs = require('secure-random-string')
 const parallel = require('run-parallel')
 function generate(args) {
 
-    let { name } = args
+    let {
+        name
+    } = args
     fs.mkdir(`./${name}`, (err) => {
         if (err) {
             return console.log(err)
@@ -14,25 +16,46 @@ function generate(args) {
         const dirstr = dirname.substr(dirname.lastIndexOf('/') + 1) + '$'
         const fixedurl = dirname.replace(new RegExp(dirstr), '');
 
-        fs.copy(`${fixedurl}/templates/entinker-boilerplate/`, `./${name}/`, (err) => {
+        const env_contents = `NODE_ENV = development
+PORT = <%= app_port %>
+DB_USER = <%= db_user %>
+DB_PASSWORD = <%= db_password  %>
+DB_HOST = <%= db_host %>
+DB = <%= db_name %>
+JWT_SECRET = <%= secret %>
+BASE_URL = http://localhost:<%= app_port  %>`
+
+        // create .env file
+        fs.writeFile(`./${name}/.env`, env_contents, (err) => {
             if (err) {
                 console.log(err)
                 process.exit()
             }
 
-            args.secret = srs({ alphanumeric: true, length: 64 })
-            let render_files = ['.env', 'package.json', '/docs/README.md']
-
-            renderFiles(render_files, args ,(err) => {
+            fs.copy(`${fixedurl}/templates/entinker-boilerplate/`, `./${name}/`, (err) => {
                 if (err) {
                     console.log(err)
                     process.exit()
                 }
-            console.log(`\nProject ${chalk.green(name)} Created. Run the following command to get started:`, `\n\n\tcd ${chalk.green(name)}`, '\n\tnpm install', '\n\tnpm run dev')
+
+                args.secret = srs({
+                    alphanumeric: true,
+                    length: 64
+                })
+                let render_files = ['.env', 'package.json', '/docs/README.md']
+
+                renderFiles(render_files, args, (err) => {
+                    if (err) {
+                        console.log(err)
+                        process.exit()
+                    }
+                    console.log(`\nProject ${chalk.green(name)} Created. Run the following command to get started:`, `\n\n\tcd ${chalk.green(name)}`, '\n\tnpm install', '\n\tnpm run dev')
+                })
             })
         })
+
     })
-} 
+}
 
 function renderFiles(filenames, data, callback) {
             let tasks = filenames.map(element => {
